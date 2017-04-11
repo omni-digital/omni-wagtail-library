1bb46a6fb0c4cdacfbee91fa3350ebcd7911729e
Aivis Abele <aabele@gmail.com>
2017-04-11 18:19:29 +0300

    Added compatibility to django<1.9

--- app/settings.py
+++ app/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
