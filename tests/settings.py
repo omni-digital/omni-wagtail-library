a1ea01b5269a01d2cdaf3ea036b832e4a63a2c6a
mitch104 <mitch_10_4@hotmail.com>
2019-02-27 17:05:22 +0000

    Add Pipfile, remove requirments file, add flake8 settings and remove MIDDLEWARE_CLASSES from test settings

--- tests/settings.py
+++ tests/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE = MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
